61290562aade51e4cbebb464237bd9e794c55033
Chris Oh <5tigerjelly@gmail.com>
2016-01-15 02:36:16 -0800

    Delete uspPersonExists.sql

--- Susan/uspPersonExists.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE uspPersonExists @FirstName varchar(30), @LastName varchar(30), @EmailAdd char(30)
-AS
-SELECT PersonID 
-FROM PERSON 
-WHERE PersonFName = @FirstName
-AND PersonLName = @LastName 
-AND EmailAddress = @EmailAdd
